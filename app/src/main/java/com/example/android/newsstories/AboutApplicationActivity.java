fd942cec9b80d0c293f2e9ecce937b4b9d135214
Greta Gri <greta.klimaite@gmail.com>
2018-05-15 14:48:33 +0300

    Added content to About aplication activity and short info in Settings activity.

--- app/src/main/java/com/example/android/newsstories/AboutApplicationActivity.java
+++ app/src/main/java/com/example/android/newsstories/AboutApplicationActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.newsstories;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AboutApplicationActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class AboutApplicationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_application);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] email = {getString(R.string.email)};
+                String subject = getString(R.string.feedback);
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, email);
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
+
--- app/src/main/java/com/example/android/newsstories/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsstories/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.android.newsstories;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
     }
 }
